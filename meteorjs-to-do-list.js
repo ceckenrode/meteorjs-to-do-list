6ec2523e96d1805900647279562f67c7d15373f4
ceckenrode <chriseckenrode@me.com>
2016-02-14 14:39:53 -0500

    added functioning checkboxes and delete buttons that update the server to list

--- meteorjs-to-do-list.js
+++ meteorjs-to-do-list.js
@@ -5,7 +5,11 @@ if (Meteor.isClient) {
     Template.body.helpers({
         tasks: function() {
             // Show newest tasks at the top
-      return Tasks.find({}, {sort: {createdAt: -1}});
+            return Tasks.find({}, {
+                sort: {
+                    createdAt: -1
+                }
+            });
         }
     });
 
@@ -29,4 +33,16 @@ if (Meteor.isClient) {
             event.target.text.value = "";
         }
     });
+
+    Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
 }
