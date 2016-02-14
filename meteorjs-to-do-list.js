39707db2406ed6e9bfb5515c3c79526e06e5569a
ceckenrode <chriseckenrode@me.com>
2016-02-14 14:24:27 -0500

    added functionality that displays list by sort order

--- meteorjs-to-do-list.js
+++ meteorjs-to-do-list.js
@@ -1,29 +1,32 @@
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
-  //This code only runs on the client
-  Template.body.helpers({
-    tasks: function() {
-      return Tasks.find({});
-    }
-  });
+    //This code only runs on the client
+    Template.body.helpers({
+        tasks: function() {
+            // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
+        }
+    });
 
-  Template.body.events({
-    "submit .new-task": function (event) {
-      // Prevent default browser form submit
-      event.preventDefault();
- 
-      // Get value from form element
-      var text = event.target.text.value;
- 
-      // Insert a task into the collection
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
- 
-      // Clear form
-      event.target.text.value = "";
-    }
-  });
+    Template.body.events({
+        "submit .new-task": function(event) {
+            // Prevent default browser form submit
+            event.preventDefault();
+
+            console.log(event);
+
+            // Get value from form element
+            var text = event.target.text.value;
+
+            // Insert a task into the collection
+            Tasks.insert({
+                text: text,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.text.value = "";
+        }
+    });
 }
